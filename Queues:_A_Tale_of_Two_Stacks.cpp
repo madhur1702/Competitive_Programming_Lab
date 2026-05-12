d8c22fa5ec1cb67b7bba0402093b1b203da5c48a
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-05-12 06:39:40 +0530

    Create Queues:_A_Tale_of_Two_Stacks.cpp

---
+++ Queues:_A_Tale_of_Two_Stacks.cpp
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> in, out;
+
+    void push(int x) {
+        in.push(x);
+    }
+
+    void pop() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        if (!out.empty())
+            out.pop();
+    }
+
+    int front() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+
+   
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
